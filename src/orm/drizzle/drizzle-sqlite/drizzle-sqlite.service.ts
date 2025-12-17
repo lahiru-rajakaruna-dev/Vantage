3eb7a8571f170194adee37f196f473d542025c4b
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2025-12-17 13:55:43 +0530

    update asynchronous methods

--- src/orm/drizzle/drizzle-sqlite/drizzle-sqlite.service.ts
+++ src/orm/drizzle/drizzle-sqlite/drizzle-sqlite.service.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import DrizzleOrm from '../drizzle-orm.service';
 import { ConfigService } from '@nestjs/config';
 import LoggerFactoryService from '../../../logger/logger-factory.service';
-import { drizzle, LibSQLDatabase } from 'drizzle-orm/libsql';
+import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './drizzle-sqlite.schema';
 import {
   clients,
@@ -33,206 +33,233 @@ export class DrizzleSqliteService extends DrizzleOrm {
     @Inject() loggerFactory: LoggerFactoryService,
   ) {
     super(configService, loggerFactory);
-    this.driver = drizzle<typeof schema>(
-      this.configService.get('SQLITE_DATABASE_URL'),
-      {
-        schema: schema,
-      },
-    );
+    const client = drizzle({
+      url: this.configService.get('SQLITE_DATABASE_URL') as string,
+    });
+    this.driver = drizzle(client, {
+      schema: schema,
+    });
   }
 
-  async addOrganization(organizationDetails: TOrganization) {
-    return this.logger.logAndReturn(
-      await this.driver
-        .insert(organizations)
-        .values(organizationDetails)
-        .returning(),
-    );
+  async addOrganization(
+    organizationDetails: TOrganization,
+  ): Promise<TOrganization> {
+    const result = await this.driver
+      .insert(organizations)
+      .values(organizationDetails)
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  updateOrganizationById(
+  async updateOrganizationById(
     organization_id: string,
     organizationUpdates: Partial<TOrganization>,
-  ) {
-    return this.logger.logAndReturn(
-      this.driver
-        .update(organizations)
-        .set(organizationUpdates)
-        .where(eq(organizations.organization_id, organization_id))
-        .returning(),
-    );
+  ): Promise<TOrganization> {
+    const result = await this.driver
+      .update(organizations)
+      .set(organizationUpdates)
+      .where(eq(organizations.organization_id, organization_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  getOrganizationDetailsById(organization_id: string) {
-    return this.logger.logAndReturn(
-      this.driver
-        .select()
-        .from(organizations)
-        .where(eq(organizations.organization_id, organization_id)),
-    );
+  async getOrganizationDetailsById(
+    organization_id: string,
+  ): Promise<TOrganization | undefined> {
+    const result = await this.driver
+      .select()
+      .from(organizations)
+      .where(eq(organizations.organization_id, organization_id));
+    return this.logger.logAndReturn(result[0]);
   }
 
-  addEmployee(employeeDetails: TEmployee) {
-    return this.logger.logAndReturn(
-      this.driver.insert(employees).values(employeeDetails).returning(),
-    );
+  async addEmployee(employeeDetails: TEmployee): Promise<TEmployee> {
+    const result = await this.driver
+      .insert(employees)
+      .values(employeeDetails)
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  viewEmployeeById(employee_id: string) {
-    return this.logger.logAndReturn(
-      this.driver
-        .select()
-        .from(employees)
-        .where(eq(employees.employee_id, employee_id)),
-    );
+  async viewEmployeeById(employee_id: string): Promise<TEmployee | undefined> {
+    const result = await this.driver
+      .select()
+      .from(employees)
+      .where(eq(employees.employee_id, employee_id));
+    return this.logger.logAndReturn(result[0]);
   }
 
-  getEmployeesByOrganizationId(organization_id: string) {
+  async getEmployeesByOrganizationId(
+    organization_id: string,
+  ): Promise<TEmployee[]> {
     return this.logger.logAndReturn(
-      this.driver
+      await this.driver
         .select()
         .from(employees)
         .where(eq(employees.employee_organization_id, organization_id)),
     );
   }
 
-  getEmployeesBySalesGroupId(sales_group_id: string) {
+  async getEmployeesBySalesGroupId(
+    sales_group_id: string,
+  ): Promise<TEmployee[]> {
     return this.logger.logAndReturn(
-      this.driver
+      await this.driver
         .select()
         .from(employees)
         .where(eq(employees.employee_sales_group_id, sales_group_id)),
     );
   }
 
-  updateEmployeeById(employee_id: string, employeeUpdates: Partial<TEmployee>) {
-    return this.logger.logAndReturn(
-      this.driver
-        .update(employees)
-        .set(employeeUpdates)
-        .where(eq(employees.employee_id, employee_id))
-        .returning(),
-    );
+  async updateEmployeeById(
+    employee_id: string,
+    employeeUpdates: Partial<TEmployee>,
+  ): Promise<TEmployee> {
+    const result = await this.driver
+      .update(employees)
+      .set(employeeUpdates)
+      .where(eq(employees.employee_id, employee_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  deleteEmployeeById(employee_id: string) {
-    return this.logger.logAndReturn(
-      this.driver
-        .delete(employees)
-        .where(eq(employees.employee_id, employee_id))
-        .returning(),
-    );
+  async deleteEmployeeById(employee_id: string): Promise<TEmployee> {
+    const result = await this.driver
+      .delete(employees)
+      .where(eq(employees.employee_id, employee_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  addItem(itemDetails: TItem) {
-    return this.logger.logAndReturn(
-      this.driver.insert(items).values(itemDetails).returning(),
-    );
+  async addItem(itemDetails: TItem): Promise<TItem> {
+    const result = await this.driver
+      .insert(items)
+      .values(itemDetails)
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  viewItemById(item_id: string) {
-    return this.logger.logAndReturn(
-      this.driver.select().from(items).where(eq(items.item_id, item_id)),
-    );
+  async viewItemById(item_id: string): Promise<TItem | undefined> {
+    const result = await this.driver
+      .select()
+      .from(items)
+      .where(eq(items.item_id, item_id));
+    return this.logger.logAndReturn(result[0]);
   }
 
-  getItemsByOrganizationId(organization_id: string) {
+  async getItemsByOrganizationId(organization_id: string): Promise<TItem[]> {
     return this.logger.logAndReturn(
-      this.driver
+      await this.driver
         .select()
         .from(items)
         .where(eq(items.item_organization_id, organization_id)),
     );
   }
 
-  updateItemById(item_id: string, itemUpdates: Partial<TItem>) {
-    return this.logger.logAndReturn(
-      this.driver
-        .update(items)
-        .set(itemUpdates)
-        .where(eq(items.item_id, item_id))
-        .returning(),
-    );
+  async updateItemById(
+    item_id: string,
+    itemUpdates: Partial<TItem>,
+  ): Promise<TItem> {
+    const result = await this.driver
+      .update(items)
+      .set(itemUpdates)
+      .where(eq(items.item_id, item_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  deleteItemById(item_id: string) {
-    return this.logger.logAndReturn(
-      this.driver.delete(items).where(eq(items.item_id, item_id)).returning(),
-    );
+  async deleteItemById(item_id: string): Promise<TItem> {
+    const result = await this.driver
+      .delete(items)
+      .where(eq(items.item_id, item_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  addSalesGroup(salesGroupDetails: TSalesGroup) {
-    return this.logger.logAndReturn(
-      this.driver.insert(salesGroups).values(salesGroupDetails).returning(),
-    );
+  async addSalesGroup(salesGroupDetails: TSalesGroup): Promise<TSalesGroup> {
+    const result = await this.driver
+      .insert(salesGroups)
+      .values(salesGroupDetails)
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  getSalesGroupsByOrganizationId(organization_id: string) {
-    return this.logger.logAndReturn(
-      this.driver
-        .select()
-        .from(salesGroups)
-        .where(eq(salesGroups.sales_group_organization_id, organization_id)),
-    );
+  async getSalesGroupsByOrganizationId(
+    organization_id: string,
+  ): Promise<TSalesGroup | undefined> {
+    const result = await this.driver
+      .select()
+      .from(salesGroups)
+      .where(eq(salesGroups.sales_group_organization_id, organization_id));
+    return this.logger.logAndReturn(result[0]);
   }
 
-  updateSalesGroupById(
+  async updateSalesGroupById(
     sales_group_id: string,
     salesGroupUpdates: Partial<TSalesGroup>,
-  ) {
-    return this.logger.logAndReturn(
-      this.driver
-        .update(salesGroups)
-        .set(salesGroupUpdates)
-        .where(eq(salesGroups.sales_group_id, sales_group_id))
-        .returning(),
-    );
+  ): Promise<TSalesGroup> {
+    const result = await this.driver
+      .update(salesGroups)
+      .set(salesGroupUpdates)
+      .where(eq(salesGroups.sales_group_id, sales_group_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  deleteSalesGroupById(sales_group_id: string) {
-    return this.logger.logAndReturn(
-      this.driver
-        .delete(salesGroups)
-        .where(eq(salesGroups.sales_group_id, sales_group_id)),
-    );
+  async deleteSalesGroupById(sales_group_id: string): Promise<TSalesGroup> {
+    const result = await this.driver
+      .delete(salesGroups)
+      .where(eq(salesGroups.sales_group_id, sales_group_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  addClient(clientDetails: TClient) {
-    return this.logger.logAndReturn(
-      this.driver.insert(clients).values(clientDetails).returning(),
-    );
+  async addClient(clientDetails: TClient): Promise<TClient> {
+    const result = await this.driver
+      .insert(clients)
+      .values(clientDetails)
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  getClientsByOrganizationId(organization_id: string) {
+  async getClientsByOrganizationId(
+    organization_id: string,
+  ): Promise<TClient[]> {
     return this.logger.logAndReturn(
-      this.driver
+      await this.driver
         .select()
         .from(clients)
         .where(eq(clients.client_organization_id, organization_id)),
     );
   }
 
-  updateClientById(client_id: string, clientUpdates: Partial<TClient>) {
-    return this.logger.logAndReturn(
-      this.driver
-        .update(clients)
-        .set(clientUpdates)
-        .where(eq(clients.client_id, client_id)),
-    );
+  async updateClientById(
+    client_id: string,
+    clientUpdates: Partial<TClient>,
+  ): Promise<TClient> {
+    const result = await this.driver
+      .update(clients)
+      .set(clientUpdates)
+      .where(eq(clients.client_id, client_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  addOrganizationPayment(paymentDetails: TOrganizationPayment) {
-    return this.logger.logAndReturn(
-      this.driver
-        .insert(organizationsPayments)
-        .values(paymentDetails)
-        .returning(),
-    );
+  async addOrganizationPayment(
+    paymentDetails: TOrganizationPayment,
+  ): Promise<TOrganizationPayment> {
+    const result = await this.driver
+      .insert(organizationsPayments)
+      .values(paymentDetails)
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  getOrganizationPaymentsByOrganizationId(organization_id: string) {
+  async getOrganizationPaymentsByOrganizationId(
+    organization_id: string,
+  ): Promise<TOrganizationPayment[]> {
     return this.logger.logAndReturn(
-      this.driver
+      await this.driver
         .select()
         .from(organizationsPayments)
         .where(
@@ -241,85 +268,97 @@ export class DrizzleSqliteService extends DrizzleOrm {
     );
   }
 
-  updateOrganizationPaymentById(
+  async updateOrganizationPaymentById(
     payment_id: string,
     paymentUpdates: Partial<TOrganizationPayment>,
-  ) {
-    return this.logger.logAndReturn(
-      this.driver
-        .update(organizationsPayments)
-        .set(paymentUpdates)
-        .where(eq(organizationsPayments.payment_organization_id, payment_id)),
-    );
+  ): Promise<TOrganizationPayment> {
+    const result = await this.driver
+      .update(organizationsPayments)
+      .set(paymentUpdates)
+      .where(eq(organizationsPayments.payment_id, payment_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  addClientPayment(paymentDetails: TClientPayment) {
-    return this.logger.logAndReturn(
-      this.driver.insert(clientsPayments).values(paymentDetails).returning(),
-    );
+  async addClientPayment(
+    paymentDetails: TClientPayment,
+  ): Promise<TClientPayment> {
+    const result = await this.driver
+      .insert(clientsPayments)
+      .values(paymentDetails)
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  getClientPaymentsByClientId(client_id: string) {
+  async getClientPaymentsByClientId(
+    client_id: string,
+  ): Promise<TClientPayment[]> {
     return this.logger.logAndReturn(
-      this.driver
+      await this.driver
         .select()
         .from(clientsPayments)
         .where(eq(clientsPayments.client_payment_client_id, client_id)),
     );
   }
 
-  updateClientPaymentById(
+  async updateClientPaymentById(
     client_payment_id: string,
     clientPaymentUpdates: Partial<TClientPayment>,
-  ) {
-    return this.logger.logAndReturn(
-      this.driver
-        .update(clientsPayments)
-        .set(clientPaymentUpdates)
-        .where(eq(clientsPayments.client_payment_id, client_payment_id))
-        .returning(),
-    );
+  ): Promise<TClientPayment> {
+    const result = await this.driver
+      .update(clientsPayments)
+      .set(clientPaymentUpdates)
+      .where(eq(clientsPayments.client_payment_id, client_payment_id))
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  addSaleItem(saleDetails: TSale) {
-    return this.logger.logAndReturn(
-      this.driver.insert(sales).values(saleDetails).returning(),
-    );
+  async addSaleItem(saleDetails: TSale): Promise<TSale> {
+    const result = await this.driver
+      .insert(sales)
+      .values(saleDetails)
+      .returning();
+    return this.logger.logAndReturn(result[0]);
   }
 
-  viewSaleById(sale_id: string) {
-    return this.logger.logAndReturn(
-      this.driver.select().from(sales).where(eq(sales.sale_id, sale_id)),
-    );
+  async viewSaleById(sale_id: string): Promise<TSale | undefined> {
+    const result = await this.driver
+      .select()
+      .from(sales)
+      .where(eq(sales.sale_id, sale_id));
+    return this.logger.logAndReturn(result[0]);
   }
 
-  getSalesByEmployeeId(employee_id: string) {
+  async getSalesByEmployeeId(employee_id: string): Promise<TSale[]> {
     return this.logger.logAndReturn(
-      this.driver
+      await this.driver
         .select()
         .from(sales)
         .where(eq(sales.sale_employee_id, employee_id)),
     );
   }
 
-  getSalesByItemId(item_id: string) {
+  async getSalesByItemId(item_id: string): Promise<TSale[]> {
     return this.logger.logAndReturn(
-      this.driver.select().from(sales).where(eq(sales.sale_item_id, item_id)),
+      await this.driver
+        .select()
+        .from(sales)
+        .where(eq(sales.sale_item_id, item_id)),
     );
   }
 
-  getSalesByOrganizationId(organization_id: string) {
+  async getSalesByOrganizationId(organization_id: string): Promise<TSale[]> {
     return this.logger.logAndReturn(
-      this.driver
+      await this.driver
         .select()
         .from(sales)
         .where(eq(sales.sale_organization_id, organization_id)),
     );
   }
 
-  getSalesByClientId(client_id: string) {
+  async getSalesByClientId(client_id: string): Promise<TSale[]> {
     return this.logger.logAndReturn(
-      this.driver
+      await this.driver
         .select()
         .from(sales)
         .where(eq(sales.sale_client_id, client_id)),
