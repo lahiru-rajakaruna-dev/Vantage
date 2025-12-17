f0d93c67044214764d48963fad028e46039a0a3d
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2025-12-17 12:54:31 +0530

    add drizzle-sqlite service

---
+++ src/orm/drizzle/drizzle-sqlite/drizzle-sqlite.schema.ts
@@ -0,0 +1,387 @@
+import {
+  sqliteTable,
+  text,
+  integer,
+  primaryKey,
+  index,
+  real,
+} from 'drizzle-orm/sqlite-core';
+import { relations } from 'drizzle-orm';
+
+export const organizations = sqliteTable(
+  'organizations',
+  {
+    organization_id: text().unique().notNull(),
+    organization_stripe_customer_id: text().unique().notNull(),
+    organization_name: text().unique().notNull(),
+    organization_registration_date: integer().notNull(),
+    organization_subscription_end_date: integer().notNull(),
+    organization_status: text({ enum: ['ACTIVE', 'DEACTIVATED'] })
+      .default('ACTIVE')
+      .notNull(),
+    organization_subscription_status: text({ enum: ['valid', 'expired'] })
+      .default('valid')
+      .notNull(),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        name: 'organization_primary_key',
+        columns: [table.organization_id, table.organization_stripe_customer_id],
+      }),
+      organizationIdIndex: index('organization_id_idx').on(
+        table.organization_id,
+      ),
+      stripeIdIndex: index('organization_stripe_customer_id_idx').on(
+        table.organization_stripe_customer_id,
+      ),
+    };
+  },
+);
+
+export const employees = sqliteTable(
+  'employees',
+  {
+    // FIX: Added .unique()
+    employee_id: text().unique().notNull(),
+    employee_organization_id: text()
+      .notNull()
+      .references(() => organizations.organization_id),
+    employee_sales_group_id: text().references(
+      () => salesGroups.sales_group_id,
+    ),
+    employee_username: text().notNull(),
+    employee_phone: text().notNull(),
+    employee_nic_number: text().notNull().unique(),
+    employee_registration_date: integer().notNull(),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        name: 'employee_primary_key',
+        columns: [table.employee_id, table.employee_organization_id],
+      }),
+      employeeIdIndex: index('employee_id_idx').on(table.employee_id),
+      organizationIdIndex: index('employee_organization_id_fk_idx').on(
+        table.employee_organization_id,
+      ),
+      salesGroupIdIndex: index('employee_sales_group_id_fk_idx').on(
+        table.employee_sales_group_id,
+      ),
+      nicIndex: index('employee_nic_number_unique_idx').on(
+        table.employee_nic_number,
+      ),
+    };
+  },
+);
+
+export const salesGroups = sqliteTable(
+  'sales_groups',
+  {
+    // FIX: Added .unique()
+    sales_group_id: text().unique().notNull(),
+    sales_group_organization_id: text()
+      .notNull()
+      .references(() => organizations.organization_id),
+    sales_group_name: text().unique().notNull(),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        name: 'sales_group_primary_key',
+        columns: [table.sales_group_id, table.sales_group_organization_id],
+      }),
+      organizationIdIndex: index('sales_group_organization_id_fk_idx').on(
+        table.sales_group_organization_id,
+      ),
+      nameIndex: index('sales_group_name_unique_idx').on(
+        table.sales_group_name,
+      ),
+    };
+  },
+);
+
+export const items = sqliteTable(
+  'items',
+  {
+    // FIX: Added .unique()
+    item_id: text().unique().notNull(),
+    item_organization_id: text()
+      .notNull()
+      .references(() => organizations.organization_id),
+    item_name: text().notNull(),
+    item_stock_unit_count: integer().default(0),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        name: 'items_primary_key',
+        columns: [table.item_id, table.item_organization_id],
+      }),
+      itemIdIndex: index('item_id_idx').on(table.item_id),
+      organizationIdIndex: index('item_organization_id_fk_idx').on(
+        table.item_organization_id,
+      ),
+    };
+  },
+);
+
+export const sales = sqliteTable(
+  'sales',
+  {
+    // FIX: Added .unique()
+    sale_id: text().unique().notNull(),
+    sale_organization_id: text()
+      .notNull()
+      .references(() => organizations.organization_id),
+    sale_employee_id: text()
+      .notNull()
+      .references(() => employees.employee_id),
+    sale_client_id: text()
+      .notNull()
+      .references(() => clients.client_id),
+    sale_client_payment_id: text()
+      .notNull()
+      .references(() => clientsPayments.client_payment_id),
+    sale_item_id: text()
+      .notNull()
+      .references(() => items.item_id),
+    sale_item_unit_count: integer().notNull().default(1),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        name: 'sales_primary_key',
+        columns: [
+          table.sale_id,
+          table.sale_organization_id,
+          table.sale_employee_id,
+          table.sale_client_id,
+          table.sale_client_payment_id,
+          table.sale_item_id,
+        ],
+      }),
+      saleIdIndex: index('sale_id_idx').on(table.sale_id),
+      organizationIdIndex: index('sale_organization_id_fk_idx').on(
+        table.sale_organization_id,
+      ),
+      employeeIdIndex: index('sale_employee_id_fk_idx').on(
+        table.sale_employee_id,
+      ),
+      clientIdIndex: index('sale_client_id_fk_idx').on(table.sale_client_id),
+      clientPaymentIdIndex: index('sale_client_payment_id_fk_idx').on(
+        table.sale_client_payment_id,
+      ),
+      itemIdIndex: index('sale_item_id_fk_idx').on(table.sale_item_id),
+    };
+  },
+);
+
+export const organizationsPayments = sqliteTable(
+  'organizations_payments',
+  {
+    // FIX: Added .unique()
+    payment_id: text().unique().notNull(),
+    payment_organization_id: text()
+      .notNull()
+      .references(() => organizations.organization_id),
+    payment_amount: real().notNull(),
+    payment_status: text({ enum: ['pending', 'paid', 'verified'] }).default(
+      'verified',
+    ),
+    payment_date: integer().notNull(),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        name: 'organization_payments_primary_key',
+        columns: [table.payment_id, table.payment_organization_id],
+      }),
+      paymentIdIndex: index('organization_payment_id_idx').on(table.payment_id),
+      organizationIdIndex: index(
+        'organization_payment_organization_id_fk_idx',
+      ).on(table.payment_organization_id),
+    };
+  },
+);
+
+export const clients = sqliteTable(
+  'clients',
+  {
+    // FIX: Added .unique()
+    client_id: text().unique().notNull(),
+    client_organization_id: text()
+      .notNull()
+      .references(() => organizations.organization_id),
+    client_stripe_customer_id: text().notNull(),
+    client_name: text().notNull(),
+    client_registration_date: integer().notNull(),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        name: 'clients_primary_key',
+        columns: [
+          table.client_id,
+          table.client_organization_id,
+          table.client_stripe_customer_id,
+        ],
+      }),
+      organizationIdIndex: index('client_organization_id_fk_idx').on(
+        table.client_organization_id,
+      ),
+      stripeIdIndex: index('client_stripe_customer_id_idx').on(
+        table.client_stripe_customer_id,
+      ),
+    };
+  },
+);
+
+export const clientsPayments = sqliteTable(
+  'client_payments',
+  {
+    // FIX: Added .unique()
+    client_payment_id: text().unique().notNull(),
+    client_payment_client_id: text()
+      .notNull()
+      .references(() => clients.client_id),
+    client_payment_organization_id: text()
+      .notNull()
+      .references(() => organizations.organization_id),
+    client_payment_amount: real().notNull(),
+    client_payment_date: integer().notNull(),
+    client_payment_status: text({ enum: ['pending', 'paid', 'verified'] })
+      .notNull()
+      .default('verified'),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        name: 'clients_payments_primary_key',
+        columns: [
+          table.client_payment_id,
+          table.client_payment_client_id,
+          table.client_payment_organization_id,
+        ],
+      }),
+      clientPaymentIdIndex: index('client_payment_id_idx').on(
+        table.client_payment_id,
+      ),
+      clientIdIndex: index('client_payment_client_id_fk_idx').on(
+        table.client_payment_client_id,
+      ),
+      organizationIdIndex: index('client_payment_organization_id_fk_idx').on(
+        table.client_payment_organization_id,
+      ),
+    };
+  },
+);
+
+// --- RELATIONS ---
+
+export const organizationsRelations = relations(organizations, ({ many }) => {
+  return {
+    employees: many(employees),
+    items: many(items),
+    salesGroups: many(salesGroups),
+    payments: many(organizationsPayments),
+    sales: many(sales),
+    clients: many(clients),
+    clientsPayments: many(clientsPayments),
+  };
+});
+
+export const employeesRelations = relations(employees, ({ one, many }) => {
+  return {
+    organization: one(organizations, {
+      fields: [employees.employee_organization_id],
+      references: [organizations.organization_id],
+    }),
+    salesGroup: one(salesGroups, {
+      fields: [employees.employee_sales_group_id],
+      references: [salesGroups.sales_group_id],
+    }),
+    sales: many(sales),
+  };
+});
+
+export const itemsRelations = relations(items, ({ one, many }) => {
+  return {
+    organization: one(organizations, {
+      fields: [items.item_organization_id],
+      references: [organizations.organization_id],
+    }),
+    sales: many(sales),
+  };
+});
+
+export const salesGroupsRelations = relations(salesGroups, ({ one, many }) => {
+  return {
+    organization: one(organizations, {
+      fields: [salesGroups.sales_group_organization_id],
+      references: [organizations.organization_id],
+    }),
+    employees: many(employees),
+  };
+});
+
+export const salesRelations = relations(sales, ({ one, many }) => {
+  return {
+    item: one(items, {
+      fields: [sales.sale_item_id],
+      references: [items.item_id],
+    }),
+    employee: one(employees, {
+      fields: [sales.sale_employee_id],
+      references: [employees.employee_id],
+    }),
+    organization: one(organizations, {
+      fields: [sales.sale_organization_id],
+      references: [organizations.organization_id],
+    }),
+    client: one(clients, {
+      fields: [sales.sale_client_id],
+      references: [clients.client_id],
+    }),
+    clientPayment: one(clientsPayments, {
+      fields: [sales.sale_client_payment_id],
+      references: [clientsPayments.client_payment_id],
+    }),
+  };
+});
+
+export const clientsRelations = relations(clients, ({ one, many }) => {
+  return {
+    organization: one(organizations, {
+      fields: [clients.client_organization_id],
+      references: [organizations.organization_id],
+    }),
+    payments: many(clientsPayments),
+    sales: many(sales),
+  };
+});
+
+export const clientsPaymentsRelations = relations(
+  clientsPayments,
+  ({ one, many }) => {
+    return {
+      client: one(clients, {
+        fields: [clientsPayments.client_payment_client_id],
+        references: [clients.client_id],
+      }),
+      organization: one(organizations, {
+        fields: [clientsPayments.client_payment_organization_id],
+        references: [organizations.organization_id],
+      }),
+    };
+  },
+);
+
+export type TOrganization = typeof organizations.$inferInsert;
+export type TEmployee = typeof employees.$inferInsert;
+export type TItem = typeof items.$inferInsert;
+export type TSalesGroup = typeof salesGroups.$inferInsert;
+export type TSale = typeof sales.$inferInsert;
+export type TClient = typeof clients.$inferInsert;
+export type TClientPayment = typeof clientsPayments.$inferInsert;
+export type TOrganizationPayment = typeof organizationsPayments.$inferInsert;
---
+++ src/orm/drizzle/drizzle-sqlite/drizzle-sqlite.service.ts
@@ -0,0 +1,325 @@
+import { Inject, Injectable } from '@nestjs/common';
+import DrizzleOrm from '../drizzle-orm.service';
+import { ConfigService } from '@nestjs/config';
+import LoggerFactoryService from '../../../logger/logger-factory.service';
+import { drizzle, LibSQLDatabase } from 'drizzle-orm/libsql';
+import * as schema from './drizzle-sqlite.schema';
+import {
+  clients,
+  clientsPayments,
+  employees,
+  items,
+  organizations,
+  organizationsPayments,
+  sales,
+  salesGroups,
+  TClient,
+  TClientPayment,
+  TEmployee,
+  TItem,
+  TOrganization,
+  TOrganizationPayment,
+  TSale,
+  TSalesGroup,
+} from '../drizzle-postgres/drizzle-postgres.schema';
+import { eq } from 'drizzle-orm';
+
+@Injectable()
+export class DrizzleSqliteService extends DrizzleOrm {
+  protected driver: ReturnType<typeof drizzle<typeof schema>>;
+
+  constructor(
+    @Inject() configService: ConfigService,
+    @Inject() loggerFactory: LoggerFactoryService,
+  ) {
+    super(configService, loggerFactory);
+    this.driver = drizzle<typeof schema>(
+      this.configService.get('SQLITE_DATABASE_URL'),
+      {
+        schema: schema,
+      },
+    );
+  }
+
+  addOrganization(organizationDetails: TOrganization) {
+    return this.logger.logAndReturn(
+      this.driver.insert(organizations).values(organizationDetails).returning(),
+    );
+  }
+
+  updateOrganizationById(
+    organization_id: string,
+    organizationUpdates: Partial<TOrganization>,
+  ) {
+    return this.logger.logAndReturn(
+      this.driver
+        .update(organizations)
+        .set(organizationUpdates)
+        .where(eq(organizations.organization_id, organization_id))
+        .returning(),
+    );
+  }
+
+  getOrganizationDetailsById(organization_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(organizations)
+        .where(eq(organizations.organization_id, organization_id)),
+    );
+  }
+
+  addEmployee(employeeDetails: TEmployee) {
+    return this.logger.logAndReturn(
+      this.driver.insert(employees).values(employeeDetails).returning(),
+    );
+  }
+
+  viewEmployeeById(employee_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(employees)
+        .where(eq(employees.employee_id, employee_id)),
+    );
+  }
+
+  getEmployeesByOrganizationId(organization_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(employees)
+        .where(eq(employees.employee_organization_id, organization_id)),
+    );
+  }
+
+  getEmployeesBySalesGroupId(sales_group_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(employees)
+        .where(eq(employees.employee_sales_group_id, sales_group_id)),
+    );
+  }
+
+  updateEmployeeById(employee_id: string, employeeUpdates: Partial<TEmployee>) {
+    return this.logger.logAndReturn(
+      this.driver
+        .update(employees)
+        .set(employeeUpdates)
+        .where(eq(employees.employee_id, employee_id))
+        .returning(),
+    );
+  }
+
+  deleteEmployeeById(employee_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .delete(employees)
+        .where(eq(employees.employee_id, employee_id))
+        .returning(),
+    );
+  }
+
+  addItem(itemDetails: TItem) {
+    return this.logger.logAndReturn(
+      this.driver.insert(items).values(itemDetails).returning(),
+    );
+  }
+
+  viewItemById(item_id: string) {
+    return this.logger.logAndReturn(
+      this.driver.select().from(items).where(eq(items.item_id, item_id)),
+    );
+  }
+
+  getItemsByOrganizationId(organization_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(items)
+        .where(eq(items.item_organization_id, organization_id)),
+    );
+  }
+
+  updateItemById(item_id: string, itemUpdates: Partial<TItem>) {
+    return this.logger.logAndReturn(
+      this.driver
+        .update(items)
+        .set(itemUpdates)
+        .where(eq(items.item_id, item_id))
+        .returning(),
+    );
+  }
+
+  deleteItemById(item_id: string) {
+    return this.logger.logAndReturn(
+      this.driver.delete(items).where(eq(items.item_id, item_id)).returning(),
+    );
+  }
+
+  addSalesGroup(salesGroupDetails: TSalesGroup) {
+    return this.logger.logAndReturn(
+      this.driver.insert(salesGroups).values(salesGroupDetails).returning(),
+    );
+  }
+
+  getSalesGroupsByOrganizationId(organization_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(salesGroups)
+        .where(eq(salesGroups.sales_group_organization_id, organization_id)),
+    );
+  }
+
+  updateSalesGroupById(
+    sales_group_id: string,
+    salesGroupUpdates: Partial<TSalesGroup>,
+  ) {
+    return this.logger.logAndReturn(
+      this.driver
+        .update(salesGroups)
+        .set(salesGroupUpdates)
+        .where(eq(salesGroups.sales_group_id, sales_group_id))
+        .returning(),
+    );
+  }
+
+  deleteSalesGroupById(sales_group_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .delete(salesGroups)
+        .where(eq(salesGroups.sales_group_id, sales_group_id)),
+    );
+  }
+
+  addClient(clientDetails: TClient) {
+    return this.logger.logAndReturn(
+      this.driver.insert(clients).values(clientDetails).returning(),
+    );
+  }
+
+  getClientsByOrganizationId(organization_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(clients)
+        .where(eq(clients.client_organization_id, organization_id)),
+    );
+  }
+
+  updateClientById(client_id: string, clientUpdates: Partial<TClient>) {
+    return this.logger.logAndReturn(
+      this.driver
+        .update(clients)
+        .set(clientUpdates)
+        .where(eq(clients.client_id, client_id)),
+    );
+  }
+
+  addOrganizationPayment(paymentDetails: TOrganizationPayment) {
+    return this.logger.logAndReturn(
+      this.driver
+        .insert(organizationsPayments)
+        .values(paymentDetails)
+        .returning(),
+    );
+  }
+
+  getOrganizationPaymentsByOrganizationId(organization_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(organizationsPayments)
+        .where(
+          eq(organizationsPayments.payment_organization_id, organization_id),
+        ),
+    );
+  }
+
+  updateOrganizationPaymentById(
+    payment_id: string,
+    paymentUpdates: Partial<TOrganizationPayment>,
+  ) {
+    return this.logger.logAndReturn(
+      this.driver
+        .update(organizationsPayments)
+        .set(paymentUpdates)
+        .where(eq(organizationsPayments.payment_organization_id, payment_id)),
+    );
+  }
+
+  addClientPayment(paymentDetails: TClientPayment) {
+    return this.logger.logAndReturn(
+      this.driver.insert(clientsPayments).values(paymentDetails).returning(),
+    );
+  }
+
+  getClientPaymentsByClientId(client_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(clientsPayments)
+        .where(eq(clientsPayments.client_payment_client_id, client_id)),
+    );
+  }
+
+  updateClientPaymentById(
+    client_payment_id: string,
+    clientPaymentUpdates: Partial<TClientPayment>,
+  ) {
+    return this.logger.logAndReturn(
+      this.driver
+        .update(clientsPayments)
+        .set(clientPaymentUpdates)
+        .where(eq(clientsPayments.client_payment_id, client_payment_id))
+        .returning(),
+    );
+  }
+
+  addSaleItem(saleDetails: TSale) {
+    return this.logger.logAndReturn(
+      this.driver.insert(sales).values(saleDetails).returning(),
+    );
+  }
+
+  viewSaleById(sale_id: string) {
+    return this.logger.logAndReturn(
+      this.driver.select().from(sales).where(eq(sales.sale_id, sale_id)),
+    );
+  }
+
+  getSalesByEmployeeId(employee_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(sales)
+        .where(eq(sales.sale_employee_id, employee_id)),
+    );
+  }
+
+  getSalesByItemId(item_id: string) {
+    return this.logger.logAndReturn(
+      this.driver.select().from(sales).where(eq(sales.sale_item_id, item_id)),
+    );
+  }
+
+  getSalesByOrganizationId(organization_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(sales)
+        .where(eq(sales.sale_organization_id, organization_id)),
+    );
+  }
+
+  getSalesByClientId(client_id: string) {
+    return this.logger.logAndReturn(
+      this.driver
+        .select()
+        .from(sales)
+        .where(eq(sales.sale_client_id, client_id)),
+    );
+  }
+}
