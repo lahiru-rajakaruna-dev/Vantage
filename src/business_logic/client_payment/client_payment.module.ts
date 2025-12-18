f2b353bc4ed7ba0fe89c7acf5bcac73383fb602a
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2025-12-18 13:12:57 +0530

    add missing OrmModule to the imports

--- src/business_logic/client_payment/client_payment.module.ts
+++ src/business_logic/client_payment/client_payment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClientPaymentService } from './client_payment.service';
 import { ClientPaymentController } from './client_payment.controller';
+import { OrmModule } from '../../orm/orm.module';
 
 @Module({
+  imports: [OrmModule],
   providers: [ClientPaymentService],
   controllers: [ClientPaymentController],
 })
--- src/business_logic/organization_payment/organization-payment.module.ts
+++ src/business_logic/organization_payment/organization-payment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrganizationPaymentController } from './organization-payment.controller';
 import { OrganizationPaymentService } from './organization-payment.service';
+import { OrmModule } from '../../orm/orm.module';
 
 @Module({
+  imports: [OrmModule],
   controllers: [OrganizationPaymentController],
   providers: [OrganizationPaymentService],
 })
