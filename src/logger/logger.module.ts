7658f6289289d6989c15b86878be0cb7695dd538
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2025-12-18 21:27:55 +0530

    updated logger module

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConsoleLoggerService } from './console-logger.service';
-import LoggerFactoryService from './logger-factory.service';
+import { ConsoleLoggerService } from './console_logger/console_logger.service';
+import { WinstonLoggerService } from './winston_logger/winston_logger.service';
+import { LoggerFactory } from './logger_factory/logger_factory.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [LoggerFactoryService, ConsoleLoggerService],
-  exports: [LoggerFactoryService],
+  imports: [ConfigModule],
+  providers: [ConsoleLoggerService, WinstonLoggerService, LoggerFactory],
+  exports: [LoggerFactory],
 })
 export class LoggerModule {}
