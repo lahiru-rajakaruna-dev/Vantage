f840ee68c9aefa5f68d00c255115539f67956484
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2025-12-18 21:36:32 +0530

    update imports list

--- src/orm/orm.module.ts
+++ src/orm/orm.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { DatabaseClientModule } from '../database-client/database-client.module';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { OrmFactory } from './orm-factory/orm-factory.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseClientModule, DrizzleModule, ConfigModule],
+  imports: [DrizzleModule, ConfigModule],
   providers: [OrmFactory],
   exports: [OrmFactory],
 })
