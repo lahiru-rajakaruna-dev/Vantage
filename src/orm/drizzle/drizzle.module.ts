5147f1b2f0a0981c6b865cf80d471c1a2ffd69bf
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2025-12-18 21:56:40 +0530

    add missing LoggerModule import

--- src/orm/drizzle/drizzle.module.ts
+++ src/orm/drizzle/drizzle.module.ts
@@ -3,9 +3,10 @@ import { DrizzlePostgresService } from './drizzle-postgres/drizzle-postgres.serv
 import { DrizzleSqliteService } from './drizzle-sqlite/drizzle-sqlite.service';
 import { DrizzleFactory } from './drizzle-factory/drizzle-factory.service';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '../../logger/logger.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, LoggerModule],
   providers: [DrizzlePostgresService, DrizzleFactory, DrizzleSqliteService],
   exports: [DrizzleFactory],
 })
