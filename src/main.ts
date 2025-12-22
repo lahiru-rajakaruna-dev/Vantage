33d69ce888ebe0339c709ef5c557ca7b5eff0377
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2025-12-22 18:39:14 +0530

    add simple application configuration check function

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as z from 'zod';
 
 async function bootstrap() {
+  checkEnv();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+async function checkEnv() {
+  const config = process.env;
+  console.debug(config);
+
+  const drizzle_postgres = z.object({
+    ['ORM_STRATEGY']: z.enum(['drizzle']),
+    ['DATABASE_STRATEGY']: z.enum(['postgres']),
+    ['POSTGRES_DATABASE_URL']: z.string(),
+  });
+
+  const drizzle_sqlite = z.object({
+    ['ORM_STRATEGY']: z.enum(['drizzle']),
+    ['DATABASE_STRATEGY']: z.enum(['sqlite']),
+    ['SQLITE_DATABASE_URL']: z.string(),
+  });
+
+  const applicationConfigStrategy = z.xor([drizzle_sqlite, drizzle_postgres]);
+
+  applicationConfigStrategy.parse(config);
+}
