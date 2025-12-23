87d073207674766dde31da928c4f0c6d8ad7134d
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2025-12-23 15:44:33 +0530

    updated application config check

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,16 @@ bootstrap();
 
 async function checkEnv() {
   const config = process.env;
-  console.debug(config);
 
   const drizzle_postgres = z.object({
+    ['NODE_ENV']: z.enum(['PRODUCTION', 'TEST']),
     ['ORM_STRATEGY']: z.enum(['drizzle']),
     ['DATABASE_STRATEGY']: z.enum(['postgres']),
     ['POSTGRES_DATABASE_URL']: z.string(),
   });
 
   const drizzle_sqlite = z.object({
+    ['NODE_ENV']: z.enum(['DEVELOPMENT']),
     ['ORM_STRATEGY']: z.enum(['drizzle']),
     ['DATABASE_STRATEGY']: z.enum(['sqlite']),
     ['SQLITE_DATABASE_URL']: z.string(),
