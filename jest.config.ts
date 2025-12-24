bf6f8a5971c53ce1ce148065c8a21e81b6359e9d
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2025-12-24 15:05:07 +0530

    separated jest config

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^uuid$': require.resolve('uuid'),
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+};
