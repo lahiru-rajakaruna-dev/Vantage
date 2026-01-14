3c06391dbb03e036536a2c1180f7d19ca7e0b0c1
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-14 11:31:24 +0530

    removed authentication & supabase modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { LoggerModule } from './logger/logger.module';
-import { OrmModule } from './orm/orm.module';
-import { DrizzleModule } from './orm/drizzle/drizzle.module';
-import { EmployeeModule } from './business_logic/employee/employee.module';
-import { OrganizationPaymentModule } from './business_logic/organization_payment/organization-payment.module';
-import { ClientPaymentModule } from './business_logic/client_payment/client_payment.module';
-import { OrganizationModule } from './business_logic/organization/organization.module';
-import { ItemModule } from './business_logic/item/item.module';
-import { SalesGroupModule } from './business_logic/sales_group/sales_group.module';
 import { ClientModule } from './business_logic/client/client.module';
+import { ClientPaymentModule } from './business_logic/client_payment/client_payment.module';
+import { EmployeeModule } from './business_logic/employee/employee.module';
+import { ItemModule } from './business_logic/item/item.module';
+import { OrganizationModule } from './business_logic/organization/organization.module';
+import { OrganizationPaymentModule } from './business_logic/organization_payment/organization-payment.module';
 import { SaleModule } from './business_logic/sale/sale.module';
+import { SalesGroupModule } from './business_logic/sales_group/sales_group.module';
+import { LoggerModule } from './logger/logger.module';
+import { DrizzleModule } from './orm/drizzle/drizzle.module';
+import { OrmModule } from './orm/orm.module';
 import { PaddleModule } from './paddle/paddle.module';
-import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
