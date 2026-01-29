478be78be908014e91eebe1f9def4eca7eb11a9f
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-29 22:07:29 +0530

    wrapped middleware with try catch

--- src/middleware/middleware.module.ts
+++ src/middleware/middleware.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { OrganizationModule } from '../business_logic/organization/organization.module';
-import { Middleware_OrganizationPuller } from './organization_puller.middleware';
+import { Module }       from '@nestjs/common';
+import {
+    OrganizationModule
+}                       from '../business_logic/organization/organization.module';
+import { LoggerModule } from '../logger/logger.module';
+import {
+    Middleware_OrganizationPuller
+}                       from './organization_puller.middleware';
+
+
 
 @Module({
-  imports: [OrganizationModule],
+  imports: [OrganizationModule, LoggerModule],
   providers: [Middleware_OrganizationPuller],
   exports: [Middleware_OrganizationPuller],
 })
--- src/middleware/organization_puller.middleware.ts
+++ src/middleware/organization_puller.middleware.ts
@@ -1,37 +1,46 @@
 import {
   Inject,
   Injectable,
+  InternalServerErrorException,
   type NestMiddleware,
   UnauthorizedException,
 } from '@nestjs/common';
-import {
-  OrganizationService
-} from '../business_logic/organization/organization.service';
-
-
+import { OrganizationService } from '../business_logic/organization/organization.service';
+import type ILoggerService from '../logger/logger.interface';
+import { TOKEN__LOGGER_FACTORY } from '../logger/logger_factory/logger_factory.service';
 
 @Injectable()
 export class Middleware_OrganizationPuller implements NestMiddleware {
   private readonly organizationService: OrganizationService;
+  private readonly logger: ILoggerService;
 
-  constructor(@Inject() organizationService: OrganizationService) {
+  constructor(
+    @Inject() organizationService: OrganizationService,
+    @Inject(TOKEN__LOGGER_FACTORY) logger: ILoggerService,
+  ) {
     this.organizationService = organizationService;
+    this.logger = logger;
   }
 
   async use(req: Request, res: Response, next: (error?: any) => void) {
-    const user_id = req['cookies']['user_id'];
+    try {
+      const user_id = req['cookies']['user_id'];
 
-    if (!user_id) {
-      throw new UnauthorizedException({
-        message: 'Unauthenticated request...',
-      });
+      if (!user_id) {
+        throw new UnauthorizedException({
+          message: 'Unauthenticated request...',
+        });
+      }
+
+      const adminsOrganization =
+        await this.organizationService.getOrganizationDetailsAdminId(user_id);
+
+      req['organization'] = adminsOrganization;
+
+      next();
+    } catch (e) {
+      this.logger.log(e);
+      throw new InternalServerErrorException();
     }
-
-    const adminsOrganization =
-      await this.organizationService.getOrganizationDetailsAdminId(user_id);
-
-    req['organization'] = adminsOrganization;
-
-    next();
   }
 }
