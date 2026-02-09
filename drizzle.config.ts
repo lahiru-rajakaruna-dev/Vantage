20dbc55367e79ced9ec61a9a76f86e9b14fdbb69
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-09 21:28:42 +0530

    fixed schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
                                 out          : './drizzle',
-                                schema       : './src/orm/drizzle/drizzle-sqlite/common.ts',
+                                schema       : './src/orm/drizzle/drizzle-sqlite/schema/index.ts',
                                 dialect      : 'sqlite',
                                 dbCredentials: {
                                     url: (process.env.SQLITE_DATABASE_URL as string) ?? 'file:./local.db',
