850ee92e6a18ae3f2acec2931eeedca8d1ce54fe
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-14 13:49:22 +0530

    add employees salaryes endpoint

---
+++ src/business_logic/employee_salary/employee_salary.controller.spec.ts
@@ -0,0 +1,31 @@
+import {
+    Test,
+    TestingModule
+}                                   from '@nestjs/testing';
+import { EmployeeSalaryController } from './employee_salary.controller';
+
+
+
+describe(
+    'EmployeeSalaryController',
+    () => {
+        let controller: EmployeeSalaryController;
+        
+        beforeEach(async () => {
+            const module: TestingModule = await Test.createTestingModule({
+                                                                             controllers: [ EmployeeSalaryController ],
+                                                                         })
+                                                    .compile();
+            
+            controller = module.get<EmployeeSalaryController>(EmployeeSalaryController);
+        });
+        
+        it(
+            'should be defined',
+            () => {
+                expect(controller)
+                    .toBeDefined();
+            }
+        );
+    }
+);
---
+++ src/business_logic/employee_salary/employee_salary.module.ts
@@ -0,0 +1,18 @@
+import { Module }                   from '@nestjs/common';
+import { LoggerModule }             from '../../logger/logger.module';
+import { OrmModule }                from '../../orm/orm.module';
+import { EmployeeSalaryController } from './employee_salary.controller';
+import { EmployeeSalaryService }    from './employee_salary.service';
+
+
+
+@Module({
+            imports    : [
+                LoggerModule,
+                OrmModule
+            ],
+            providers  : [ EmployeeSalaryService ],
+            controllers: [ EmployeeSalaryController ]
+        })
+export class EmployeeSalaryModule {
+}
